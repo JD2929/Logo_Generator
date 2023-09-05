f83651f9391a6e95b0bf142be6959c9b759e33a6
JD2929 <jandunk@creonova.com>
2023-09-05 20:20:02 -0300

    Complete Pseudocode.

---
+++ test/test.js
@@ -0,0 +1,9 @@
+//Each shape class should be tested for a `render()` method that returns a string for the corresponding SVG file with the given shape color.
+
+//run test at the constructor level to ensure the whole logo produces the expected result
+
+//specifically make sure that the code can pass this test 
+
+/*const shape = new Triangle();
+shape.setColor("blue");
+expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />'); */
