453d31ea78c974b1dadf829323bae7f97a206794
JD2929 <jandunk@creonova.com>
2023-09-09 17:19:08 -0300

    Integrated prompts to logo generation

--- index.js
+++ index.js
@@ -2,28 +2,44 @@
 import fs from 'fs' 
 
 //import all needed files
+import {Triangle} from "./lib/triangle.js";
+import {Circle} from "./lib/circle.js";
 import {Square} from "./lib/square.js";
-import {Logo} from "./lib/logo.js"
+import {Logo} from "./lib/logo.js";
+import {askPrompts} from "./lib/prompt.js";
 
-//use prompt class to gather desired configuration
+//use prompt function to gather desired configuration
 
 //instantiate the shape requested by the user
 function createLogo () {
-const shapeRequested = new Square ("red");
+askPrompts ()
+.then ( ({characters, textColor, shapeChoice, shapeColor}) => {
+console.log (`${characters} "got here"`)
 
-const logo = new Logo (shapeRequested)
+let shapeRequested = null
+
+if (shapeChoice === "triangle") {
+  shapeRequested = new Triangle (shapeColor)
+}else if (shapeChoice === "square") {
+  shapeRequested = new Square (shapeColor)
+} else if (shapeChoice === "circle") {
+  shapeRequested = new Circle (shapeColor)
+} 
+
+const logo = new Logo (shapeRequested, characters, textColor)
 
 //render to a string using the logo object
 const logoSVGstring = logo.render ()
-fs.writeFile ('./logo.svg',logoSVGstring, (err) => {
+fs.writeFile ('./logo.svg', logoSVGstring, (err) => {
     if (err)
       console.log(err)})
-}
-//if shape = 'triangle' then shape = new Triangle
 
+} )
+
+  
 //instatiate the construct class (it will send the shape object, the text and color info)
 
-
+}
 
 //generate logo
 createLogo ();
